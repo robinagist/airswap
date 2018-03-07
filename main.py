f490a60a5c71ad0ccfca5f24138119a9acc70de4
= <=>
2018-03-07 10:23:42 -0800

    fixed output at end of data loading stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from utils import mongo_connect, create_deques, add_new_ba_avg, get_moving_average, get_data, persist, prip
+from utils import mongo_connect, create_deques, add_new_ba_avg, get_moving_average, persist, prip
 from multiprocessing import Process
-from threading import Thread
 import config, time, handlers, sys
 
 
@@ -27,10 +26,12 @@ def moving_average(db, cur1, cur2, exch, url, handler_name):
 
         sample_count += 1
         pair = "{}_{}".format(cur1, cur2)
-        if sample_count >= max_deque_size:
+        if sample_count > max_deque_size:
             print("pair: {} exch: {} bid_avg: {} ask_avg: {}".format(pair, exch, b_ma, a_ma ))
+        elif sample_count == max_deque_size:
+            prip("")
         else:
-            prip("need {} data samples to compute moving average".format(max_deque_size - sample_count))
+            prip("need {} more data samples to compute moving average".format(max_deque_size - sample_count))
 
         if config.PERSIST:
             persist(db, exch, pair, b_ma, a_ma)
@@ -47,20 +48,19 @@ if __name__ == '__main__':
     cur1 = sys.argv[1]
     cur2 = sys.argv[2]
 
-    # if config.PERSIST == True
     # create a connection to mongo to persist time series data
     db = mongo_connect(config.MONGO_HOST) if config.PERSIST else None
     jobs = []
     # for each exchange, create
     for ex in config.exchanges:
-        exname = ex["name"]
-        exhandler = ex["handler"]
-        # if bypass is set to 1 or true, bypass this exchange
+        # if bypass is set to true, bypass this exchange
         if ex["bypass"]:
             continue
+        name = ex["name"]
+        handler = ex["handler"]
         url = ex["url"]
 
-        p = Process(target=moving_average, args=(db, cur1, cur2, exname, url, exhandler))
+        p = Process(target=moving_average, args=(db, cur1, cur2, name, url, handler))
         p.daemon = True
         jobs.append(p)
         p.start()
